d3082afd86d0abc3c7c7e03830518fba2dc910ef
Jesper Kolbeck <kolbeckxyz@outlook.com>
2020-10-05 21:42:54 +0200

    Fetch beer information from API

--- src/components/List.js
+++ src/components/List.js
@@ -6,21 +6,41 @@ import Item from '../components/Item';
 
 class List extends Component {
     state = {
-        items: [{id: 1, text: 'Start'}],
+        isLoaded: false,
+        items: [],
         searchTerm: ''
     }
 
     getItems = () => {
-        this.setState((state) => {
-            return {items: [{id: 1, text: state.searchTerm}]}
-        });
+        fetch("https://api.punkapi.com/v2/beers?beer_name=ipa&page=1&per_page=3")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+        // this.setState((state) => {
+        //     return { items: [{ id: 1, text: state.searchTerm }] }
+        // });
     }
 
     onSearchInputChange = (event) => {
         if (event.target.value) {
-            this.setState({searchTerm: event.target.value});
+            this.setState({ searchTerm: event.target.value });
         } else {
-            this.setState({searchTerm: 'Tadaaaa...'});
+            this.setState({ searchTerm: 'Tadaaaa...' });
         }
         this.getItems();
     }
@@ -30,21 +50,21 @@ class List extends Component {
             <div>
                 { this.state.items ? (
                     <div>
-                        <TextField style={{padding: 24}}
+                        <TextField style={{ padding: 24 }}
                             id="searchInput"
                             placeholder="Search Term"
                             margin="normal"
                             onChange={this.onSearchInputChange}
-                            />
-                        <Grid container style={{padding: 24}}>
-                            { this.state.items.map((item, i) => (
-                                <Grid item key={i} xs={12} sm={6} lg={4} xl={3}>
-                                    <Item item={item}/>
+                        />
+                        <Grid container style={{ padding: 24 }}>
+                            {this.state.items.map((item) => (
+                                <Grid item key={item.id} xs={12} sm={6} lg={4} xl={3}>
+                                    <Item item={item} />
                                 </Grid>
                             ))}
                         </Grid>
                     </div>
-                ) : "Nada" }
+                ) : "Nada"}
             </div>
         );
     }
